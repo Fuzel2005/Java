d566f6b9340a3d2d2686e14d3bf3cba6e6fa44f5
Fuzel25 <fuzelrahaman@gmail.com>
2025-07-10 23:04:32 +0530

    Add palindrome_number.java to check if a number is a palindrome

---
+++ palindrome_number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class palindrome_number {
+    public static boolean isPalindrome(int number) {
+        int reversed = 0;
+        int original = number;
+        while (number > 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+}
