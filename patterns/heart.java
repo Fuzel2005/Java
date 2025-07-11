f5630245bbb68b1295646c4d3b0a47b26372865e
Fuzel25 <fuzelrahaman@gmail.com>
2025-07-12 02:21:12 +0530

    Add heart.java to print a heart pattern using asterisks

---
+++ patterns/heart.java
@@ -0,0 +1,35 @@
+package patterns;
+
+public class heart {
+    public static void main(String[] args) {
+        int n = 6; // Size of the heart
+
+        // Upper part
+        for (int i = n / 2; i <= n; i += 2) {
+            for (int j = 1; j < n - i; j += 2) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
