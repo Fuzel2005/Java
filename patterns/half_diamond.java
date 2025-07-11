f13b47cb1d10dc910dbf07897c38b19282403027
Fuzel25 <fuzelrahaman@gmail.com>
2025-07-12 02:21:27 +0530

    Add half_diamond.java to print a half diamond pattern using asterisks

---
+++ patterns/half_diamond.java
@@ -0,0 +1,18 @@
+package patterns;
+
+public class half_diamond {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows for the half diamond pattern
+
+        // Upper part of the half diamond
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
